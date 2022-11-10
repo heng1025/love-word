f131b6097f9dc79695181d4e2f4bff336ce9fd1f
iron <iron.cheng@qq.com>
2022-11-10 22:40:00 +0800

    fix: content_script `rem` value is controlled by outside html

---
+++ postcss-replace.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('postcss').PluginCreator}
+ */
+module.exports = (opts = { baseValue: 16 }) => {
+  return {
+    postcssPlugin: "replace-root",
+    Rule(rule) {
+      rule.selector = rule.selector.replace(/:root/, ":root,:host");
+    },
+    // rem->px (content script rem is controlled by outside html)
+    Declaration(decl) {
+      const unit = "px";
+      const re = /"[^"]+"|'[^']+'|url\([^)]+\)|(-?\d*\.?\d+)rem/g;
+      decl.value = decl.value.replace(re, (match, p1) => {
+        if (p1 === undefined) return match;
+
+        return `${p1 * opts.baseValue}${p1 == 0 ? "" : unit}`;
+      });
+    },
+  };
+};
+
+module.exports.postcss = true;
