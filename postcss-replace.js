9dac718fa793826b6dc1e91de445ced3133b40df
iron <iron.cheng@qq.com>
2022-12-16 20:23:21 +0800

    fix:  fire many times

--- postcss-replace.js
+++ postcss-replace.js
@@ -5,7 +5,10 @@ module.exports = (opts = { baseValue: 16 }) => {
   return {
     postcssPlugin: "replace-root",
     Rule(rule) {
-      rule.selector = rule.selector.replace(/:root/, ":root,:host");
+      // Declaration will fire this
+      if (/:root/.test(rule.selector) && !/:host/.test(rule.selector)) {
+        rule.selector = rule.selector.replace(/^(:root),?/, "$1,:host");
+      }
     },
     // rem->px (content script rem is controlled by outside html)
     Declaration(decl) {
